f9ffb915222509c593e416716a69aca23226b736
870751720 <870751720@qq.com>
2024-09-10 00:32:51 +0800

    gggg

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,9 @@ if ! command -v docker &> /dev/null; then
   sudo yum remove -y docker-ce docker-ce-cli containerd.io
   sudo rm -rf /var/lib/docker
   sudo rm -rf /var/run/docker
+  sudo rm /etc/yum.repos.d/docker-ce.repo
   sudo yum install -y yum-utils
-  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  sudo yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
   sudo yum install -y docker-ce docker-ce-cli containerd.io
   sudo systemctl start docker
   sudo systemctl enable docker
