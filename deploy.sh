7a1d2947d7e0ffc5ea842ae921cad494dcfd12e6
870751720 <870751720@qq.com>
2024-09-10 00:20:51 +0800

    dcxc

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ if ! command -v docker &> /dev/null; then
   sudo rm -rf /var/lib/docker
   sudo rm -rf /var/run/docker
   sudo yum install -y yum-utils
-  sudo yum-config-manager --add-repo https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/rhel/docker-ce.repo
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
   sudo yum install -y docker-ce docker-ce-cli containerd.io
   sudo systemctl start docker
   sudo systemctl enable docker
