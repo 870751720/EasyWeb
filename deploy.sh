d6bba30221b5b0f3381715c4592b8ed1007716fe
870751720 <870751720@qq.com>
2024-09-09 23:53:59 +0800

    dddd

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,12 @@ IMAGE_NAME=$1
 
 
 if ! command -v docker &> /dev/null; then
-
+  sudo yum remove -y docker-ce docker-ce-cli containerd.io
+  sudo rm -rf /var/lib/docker
+  sudo rm -rf /var/run/docker
   sudo yum install -y yum-utils
-
   sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
   sudo yum install -y docker-ce docker-ce-cli containerd.io
-
   sudo systemctl start docker
   sudo systemctl enable docker
 fi
