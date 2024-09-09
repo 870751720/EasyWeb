f2052aea05ec3d3097a6ab9f93a39b2bcef864ef
870751720 <870751720@qq.com>
2024-09-09 23:35:13 +0800

    ddddsd

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 IMAGE_NAME=$1
 
+
 if ! command -v docker &> /dev/null; then
-  sudo apt update
-  sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
-  sudo apt update
-  sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+  sudo yum install -y yum-utils
+
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+  sudo yum install -y docker-ce docker-ce-cli containerd.io
+
   sudo systemctl start docker
   sudo systemctl enable docker
 fi
 
 docker login ghcr.io -u $USER_NAME -p $DOCKER_TOKEN
+
 docker pull ghcr.io/$USER_NAME/$IMAGE_NAME:latest
 
 docker stop $IMAGE_NAME || true
 docker rm $IMAGE_NAME || true
+
 docker run -d --name $IMAGE_NAME -p 80:5000 ghcr.io/$USER_NAME/$IMAGE_NAME:latest
