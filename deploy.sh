4067974362700d031483cee4f6c8da21a7370377
870751720 <870751720@qq.com>
2024-09-09 22:14:17 +0800

    #am

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 IMAGE_NAME=$1
 DAEMON_JSON=$2
+
 if ! command -v docker &> /dev/null; then
   sudo apt update
   sudo mkdir -p /etc/docker
-  echo '$DAEMON_JSON' | sudo tee /etc/docker/daemon.json
-  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-  sudo yum install -y docker-ce docker-ce-cli containerd.io
+  echo "$DAEMON_JSON" | sudo tee /etc/docker/daemon.json
+  sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+  sudo apt update
+  sudo apt install -y docker-ce docker-ce-cli containerd.io
   sudo systemctl start docker
   sudo systemctl enable docker
 fi
-docker load -i /tmp/$IMAGE_NAME.tar.gz
+
+docker login ghcr.io -u $USER_NAME -p $GITHUB_TOKEN
+docker pull ghcr.io/$USER_NAME/$IMAGE_NAME:latest
+
 docker stop $IMAGE_NAME || true
 docker rm $IMAGE_NAME || true
-docker run -d --name $IMAGE_NAME -p 80:5000 $IMAGE_NAME
+docker run -d --name $IMAGE_NAME -p 80:5000 ghcr.io/$USER_NAME/$IMAGE_NAME:latest
