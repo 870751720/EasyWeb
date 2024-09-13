fa59cf2f346526dca0d8424672bf86e9786f0ca3
hejiawen <hejiawen@corp.netease.com>
2024-09-13 12:03:38 +0800

    cxcx

--- README.md
+++ README.md
@@ -5,7 +5,18 @@
 3 需要在github的Setting-Security-Secrets and Variables-Actions里面添加一个Repository SSH_PRIVATE_KEY的密钥,并且你的密钥变了这个也需要变,并且这个密钥新服务器也是用它
 4 每次触发需要push tag上去
 
+
+# 新UI
+# 安装 Nvm
+1 
+https://github.com/coreybutler/nvm-windows/releases ->Assets->nvm-setup.exe
+
+2
+nvm help can see all
+nvm install 22.1.0
+nvm use 22.1.0
+3
+npx create-react-app easy_react
 # todo
-添加数据库
 添加图片视频资源
 以及对资源的上传保存等
--- project/code/app.py
+++ project/code/app.py
@@ -1,17 +1,22 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from blueprints import register_blueprints
 from db.db import init_db
 from errors import init_error
 
 
 def create_app():
-	app = Flask(__name__)
+	app = Flask(__name__, static_folder="./statics", static_url_path="/")
+
+	@app.route("/")
+	def server():
+		return send_from_directory(app.static_folder, "index.html")
+
 	init_db(app)
 	init_error(app)
 	register_blueprints(app)
 	return app
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	app = create_app()
-	app.run(host='0.0.0.0')
+	app.run(host="0.0.0.0")
