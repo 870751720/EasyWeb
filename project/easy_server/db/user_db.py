b6c845ce03a9dd98b5d9732bf32dbf6b5a9dacf5
870751720 <870751720@qq.com>
2024-09-15 19:32:30 +0800

    dddff

--- project/easy_server/db/user_db.py
+++ project/easy_server/db/user_db.py
@@ -4,7 +4,7 @@ from db.db import db
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(80), nullable=False)
-	password = db.Column(db.String(120), nullable=False)
+	password = db.Column(db.String(255), nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=True)
 	role = db.Column(db.String(20), default="guest")  # guest user admin superadmin
 
