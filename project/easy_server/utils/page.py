b4e2b1b3a57104bb6b9e38cc957a6693d664fd56
870751720 <870751720@qq.com>
2024-09-18 00:47:27 +0800

    ddsa

--- project/easy_server/utils/page.py
+++ project/easy_server/utils/page.py
@@ -1,3 +1,4 @@
+import os
 from flask import jsonify, send_from_directory, request, g
 from utils.localize import _l
 
@@ -8,8 +9,11 @@ def init_page(app):
 		language = request.headers.get("Accept-Language", "ch")
 		g.language = language
 
-	@app.route("/")
-	def server():
+	@app.route("/", defaults={"path": ""})
+	@app.route("/<path:path>")
+	def serve_react_app(path):
+		if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+			return send_from_directory(app.static_folder, path)
 		return send_from_directory(app.static_folder, "index.html")
 
 	@app.errorhandler(404)
