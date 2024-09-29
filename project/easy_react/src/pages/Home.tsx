cce415c5e5322c9b9cf25e70730ec0cbe16bde20
870751720 <870751720@qq.com>
2024-09-30 01:49:25 +0800

    cc

--- project/easy_react/src/pages/Home.tsx
+++ project/easy_react/src/pages/Home.tsx
@@ -1,9 +1,8 @@
-import { useRequest } from 'ahooks';
-import React, { useEffect, useState } from 'react';
-import { fetchGet, resUrl } from '../utils/netUtil';
-import { Card, Typography } from 'antd';
-const { Text } = Typography;
-
+import React, { useEffect, useState } from "react";
+import { useRequest } from "ahooks";
+import { Card } from "antd";
+import { fetchGet, resUrl } from "../utils/netUtil";
+import "./Home.css";
 
 const Home: React.FC = () => {
     interface Recommend {
@@ -12,6 +11,8 @@ const Home: React.FC = () => {
     }
 
     const [recommend, setRecommend] = useState<Recommend>();
+    const [displayedText, setDisplayedText] = useState("");
+
     const { run: fetchRecommendGetRequest } = useRequest(
         () => fetchGet("/recommend/get"),
         {
@@ -29,26 +30,55 @@ const Home: React.FC = () => {
         fetchRecommendGetRequest();
     }, [fetchRecommendGetRequest]);
 
+    useEffect(() => {
+        if (recommend?.res_type === "txt" && recommend?.res_info) {
+            let currentIndex = -1;
+            const intervalId = setInterval(() => {
+                if (currentIndex + 1 < recommend.res_info.length) {
+                    setDisplayedText((prev) => prev + recommend.res_info[currentIndex]);
+                    currentIndex++;
+                    console.log(currentIndex, recommend.res_info[currentIndex]);
+                } else {
+                    clearInterval(intervalId);
+                }
+            }, 100);
+
+            return () => clearInterval(intervalId);
+        }
+    }, [recommend]);
+
     const renderContent = () => {
         if (!recommend) return null;
 
         switch (recommend.res_type) {
-            case 'txt':
+            case "txt":
                 return (
-                    <Card style={{ width: 600 }}>
-                        <Text>{recommend.res_info}</Text>
+                    <Card style={{ width: 800 }}>
+                        <div className="styled-text">{displayedText}</div>
                     </Card>
                 );
-            case 'img':
+            case "img":
                 return (
-                    <Card style={{ width: 600 }}>
-                        <img src={resUrl(recommend.res_info)} alt="img" style={{ width: '100%' }} />
+                    <Card style={{ width: 800 }}>
+                        <img
+                            src={resUrl(recommend.res_info)}
+                            alt="img"
+                            style={{
+                                width: "100%",
+                                maxHeight: "600px",
+                                objectFit: "cover",
+                            }}
+                        />
                     </Card>
                 );
-            case 'video':
+            case "video":
                 return (
-                    <Card style={{ width: 600 }}>
-                        <video src={recommend.res_info} controls style={{ width: '100%' }} />
+                    <Card style={{ width: 800 }}>
+                        <video
+                            src={resUrl(recommend.res_info)}
+                            controls
+                            style={{ width: "100%" }}
+                        />
                     </Card>
                 );
             default:
@@ -57,7 +87,15 @@ const Home: React.FC = () => {
     };
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '20vh' }}>
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                height: "100vh",
+                paddingTop: "50px",
+            }}
+        >
             {renderContent()}
         </div>
     );
