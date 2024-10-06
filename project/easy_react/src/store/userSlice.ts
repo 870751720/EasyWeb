7510af9074ea0b8db973cbf8576a3f9603ee500b
870751720 <870751720@qq.com>
2024-10-06 20:39:53 +0800

    Refactor CSS formatting in Admin.css, Home.css, Head.css, and index.css to use consistent tab spacing and adjust indentation styles for clarity and conformity.

--- project/easy_react/src/store/userSlice.ts
+++ project/easy_react/src/store/userSlice.ts
@@ -1,32 +1,31 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 interface UserInfo {
-  name: string;
-  email: string;
+	name: string
+	email: string
 }
 
 interface UserState {
-  userInfo: UserInfo | null;
+	userInfo: UserInfo | null
 }
 
 const initialState: UserState = {
-  userInfo: null,
-};
-
+	userInfo: null
+}
 
 const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    setUserInfo: (state, action: PayloadAction<UserInfo>) => {
-      state.userInfo = action.payload;
-    },
-    clearUserInfo: (state) => {
-      state.userInfo = null;
-    },
-  },
-});
+	name: "user",
+	initialState,
+	reducers: {
+		setUserInfo: (state, action: PayloadAction<UserInfo>) => {
+			state.userInfo = action.payload
+		},
+		clearUserInfo: (state) => {
+			state.userInfo = null
+		}
+	}
+})
 
-export const { setUserInfo, clearUserInfo } = userSlice.actions;
+export const { setUserInfo, clearUserInfo } = userSlice.actions
 
-export default userSlice.reducer;
+export default userSlice.reducer
