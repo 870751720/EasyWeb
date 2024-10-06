3b0a998875ec3cc1f691319bbb4eea70206ccd95
870751720 <870751720@qq.com>
2024-10-06 19:35:31 +0800

    更新 VSCode 配置和 Python 应用程序初始化逻辑，添加扩展推荐，改进格式设置选项

--- project/easy_server/app.py
+++ project/easy_server/app.py
@@ -5,17 +5,20 @@ from db.db import init_db
 from utils.page import init_page
 from utils.mail import init_mail
 
+
 def create_app():
-	app = Flask(__name__, static_folder="./statics", static_url_path="/")
+    app = Flask(__name__, static_folder="./statics", static_url_path="/")
 
-	init_mail(app)
-	init_blueprints(app)
-	init_page(app)
-	init_db(app)
-	CORS(app)
+    init_mail(app)
 
-	return app
+    init_blueprints(app)
+    init_page(app)
+    init_db(app)
+    CORS(app)
+
+    return app
+
 
 if __name__ == "__main__":
-	app = create_app()
-	app.run(host="0.0.0.0")
+    app = create_app()
+    app.run(host="0.0.0.0")
