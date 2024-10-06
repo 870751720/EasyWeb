e0e5d27f3d78a4f5a7c26704b919849470c2afe7
870751720 <870751720@qq.com>
2024-10-06 20:33:29 +0800

    优化代码格式，统一使用双引号并调整缩进风格

--- project/easy_react/src/App.tsx
+++ project/easy_react/src/App.tsx
@@ -1,22 +1,26 @@
-import React, { Suspense } from 'react';
-import { Navigate, Route, HashRouter as Router, Routes } from 'react-router-dom';
-import Header from './components/Head';
-import routes from './routes';
+import React, { Suspense } from "react"
+import { Navigate, Route, HashRouter as Router, Routes } from "react-router-dom"
+import Header from "./components/Head"
+import routes from "./routes"
 
 const App: React.FC = () => {
-    return (
-        <Router>
-            <Header />
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    {routes.map((route) => (
-                        <Route key={route.path} path={route.path} element={route.element} />
-                    ))}
-                    <Route path="*" element={<Navigate to="/" />} />
-                </Routes>
-            </Suspense>
-        </Router>
-    );
-};
+	return (
+		<Router>
+			<Header />
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					{routes.map((route) => (
+						<Route
+							key={route.path}
+							path={route.path}
+							element={route.element}
+						/>
+					))}
+					<Route path="*" element={<Navigate to="/" />} />
+				</Routes>
+			</Suspense>
+		</Router>
+	)
+}
 
-export default App;
+export default App
