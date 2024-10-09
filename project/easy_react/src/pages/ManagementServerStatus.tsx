a9616d0ef812c562afaa838ce9137bedc82e474c
870751720 <870751720@qq.com>
2024-10-10 00:02:33 +0800

    ccc

--- project/easy_react/src/pages/ManagementServerStatus.tsx
+++ project/easy_react/src/pages/ManagementServerStatus.tsx
@@ -1,10 +1,12 @@
 import { useRequest } from "ahooks"
-import { Button, Card, Col, Row, Statistic } from "antd"
-import React, { useEffect, useState } from "react"
+import { Card, Col, Row, Statistic } from "antd"
+import React, { useEffect, useRef, useState } from "react"
+import _l from "../utils/i18n"
 import { fetchGet } from "../utils/netUtil"
 
 const ManagementServerStatus: React.FC = () => {
 	const [serverStatus, setServerStatus] = useState<any>(null)
+	const ref = useRef<HTMLDivElement>(null)
 	const { run: fetchServerStatusRequest } = useRequest(
 		() => fetchGet("/server_status/get"),
 		{
@@ -16,54 +18,99 @@ const ManagementServerStatus: React.FC = () => {
 	)
 
 	useEffect(() => {
-		fetchServerStatusRequest()
+		const observer = new IntersectionObserver(([entry]) => {
+			if (entry.isIntersecting) {
+				fetchServerStatusRequest()
+				const interval = setInterval(fetchServerStatusRequest, 1000)
+
+				return () => clearInterval(interval)
+			}
+		})
+
+		if (ref.current) {
+			observer.observe(ref.current)
+		}
+
+		return () => {
+			if (ref.current) {
+				observer.unobserve(ref.current)
+			}
+		}
 	}, [fetchServerStatusRequest])
 
 	return (
 		<div>
-			<Button
-				onClick={fetchServerStatusRequest}
-				type="primary"
-				style={{ marginBottom: 16 }}
-			>
-				刷新状态
-			</Button>
 			{serverStatus && (
 				<Row gutter={16}>
-					<Col span={8}>
-						<Card title="内存" bordered={false}>
+					<Col span={6}>
+						<Card title={_l.TID_COMMON_MEMORY} bordered={false}>
 							<Statistic
-								title="总内存"
-								value={serverStatus.memory.total}
-								suffix="字节"
+								title={_l.TID_COMMON_TOTAL}
+								value={
+									serverStatus.memory.total / (1024 * 1024)
+								}
+								suffix="MB"
 							/>
 							<Statistic
-								title="可用内存"
-								value={serverStatus.memory.available}
-								suffix="字节"
+								title={_l.TID_COMMON_AVAILABLE}
+								value={
+									serverStatus.memory.available /
+									(1024 * 1024)
+								}
+								suffix="MB"
+							/>
+							<Statistic
+								title={_l.TID_COMMON_USED}
+								value={serverStatus.memory.used / (1024 * 1024)}
+								suffix="MB"
+							/>
+							<Statistic
+								title={_l.TID_COMMON_USE_PERCENT}
+								value={serverStatus.memory.percent}
+								suffix="%"
 							/>
 						</Card>
 					</Col>
-					<Col span={8}>
+					<Col span={6}>
 						<Card title="CPU" bordered={false}>
 							<Statistic
-								title="使用率"
+								title={_l.TID_COMMON_USE_PERCENT}
 								value={serverStatus.cpu.percent}
 								suffix="%"
 							/>
 						</Card>
 					</Col>
-					<Col span={8}>
-						<Card title="磁盘" bordered={false}>
+					<Col span={6}>
+						<Card title={_l.TID_COMMON_DISK} bordered={false}>
 							<Statistic
-								title="总磁盘"
-								value={serverStatus.disk.total}
-								suffix="字节"
+								title={_l.TID_COMMON_TOTAL}
+								value={serverStatus.disk.total / (1024 * 1024)}
+								suffix="MB"
 							/>
 							<Statistic
-								title="已用磁盘"
-								value={serverStatus.disk.used}
-								suffix="字节"
+								title={_l.TID_COMMON_AVAILABLE}
+								value={serverStatus.disk.used / (1024 * 1024)}
+								suffix="MB"
+							/>
+						</Card>
+					</Col>
+					<Col span={6}>
+						<Card title={_l.TID_COMMON_NETWORK} bordered={false}>
+							<Statistic
+								title={_l.TID_COMMON_SENT}
+								value={
+									serverStatus.network.bytes_sent /
+									(1024 * 1024)
+								}
+								suffix="MB"
+							/>
+							<Statistic
+								title={_l.TID_COMMON_RECEIVED}
+								value={
+									serverStatus.network.bytes_recv /
+									(1024 * 1024)
+								}
+								suffix="MB"
 							/>
 						</Card>
 					</Col>
